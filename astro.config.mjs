80c856c120385443fe8b728fec325468b440d042
vhemalheiro <victorhugomalheiro@gmail.com>
2025-10-06 17:56:07 -0300

    feat: config site options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import site from 'c:/Repositories/github-action-site/site.json';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {    
-      plugins: [tailwindcss()],  
-    },
+  site: site.page,
+  vite: {    
+    plugins: [tailwindcss()],  
+  },
 });
